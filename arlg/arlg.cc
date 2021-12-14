70e636a0450a7ae49ee389812d4a507ab2979201
Jerry Wu <j553wu@uwaterloo.ca>
2021-12-14 22:55:58 +0000

    Added first level

--- arlg/arlg.cc
+++ arlg/arlg.cc
@@ -7,8 +7,8 @@
 #include "popup.h"
 #include "stalker.h"
 #include "snake.h"
+#include "point.h"
 #include <string>
-
 
 void ARLG::spawnWalker(int x, int y) {
     Entity* enemy = queueSpawn(std::make_unique<Walker>(), 1);
@@ -40,11 +40,55 @@ void ARLG::loadLevel() {
     if (p) playerhp = p->gethp();
     queueDestroyAll();
     p = queueSpawn(std::make_unique<Player>(), 1);
+    std::list<Point> pos = {{39, 18}, {13, 8}, {57, 12}, {21, 4}, {29, 16}, {17, 16}, {71, 12}, {63, 4}}
     p->heal(playerhp);
-    p->setPosition(39, 18);
+    size_t ppos = rand() % pos.size();
+    pos.erase(ppos);
+    p->setPosition(pos[rand() % 8]);
     e = queueSpawn(std::make_unique<Exit>(), 1);
-    e->setPosition(12, 12);
+    size_t epos = rand() % pos.size();
+    e->setPosition(pos[epos]);
+    if (level == 1) level1();
+    else if (level == 2) level2();
+    else if (level == 3) level3();
+    else if (level == 4) level4();
+    else if (level == 5) level5();
+    else if (level == 6) level6();
+}
 
+void ARLG::level1() {
+    std::list<Point> pos;
+    for (int x = 6; x < 74; x += 4) {
+        for (int y = 4; y < 16; y += 4) {
+            pos.push_back(Point{x, y});
+        }
+    }
+    for (int i = 0; i < 6; ++i) {
+        size_t temp = rand() % pos.size();
+        spos = pos[temp];
+        pos.erase(temp);
+        spawnWalker(spos.x, spos.y);
+    }
+}
+
+void ARLG::level2() {
+    
+}
+
+void ARLG::level3() {
+    
+}
+
+void ARLG::level4() {
+    
+}
+
+void ARLG::level5() {
+    
+}
+
+void ARLG::level6() {
+    
 }
 
 void ARLG::initialize(Game &game) {
@@ -64,6 +108,7 @@ void ARLG::process(Game &game) {
         e->openExit();
     }
     if (e->isExited()) {
+        if (level > 6) game.stop();
         ++level;
         loadLevel();
     }
